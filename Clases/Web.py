2fe7253be8e5b7ca4c3c771101c79f8c2c8de648
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-22 10:36:45 +0100

    arreglados los prints, pero sigue sin funcionar correctamente

--- Clases/Web.py
+++ Clases/Web.py
@@ -7,25 +7,17 @@ class Correo_electronico:
     def comprobar_hora():
         hora_actual=str(datetime.now().time())
         if hora_actual > "08:00:00.000000" and hora_actual < "12:00:00.000000":
-            print("Buenos días:\n Introduzca un usuario y un correo eléctronico para acceder:")
+            print("Buenos días:\nIntroduzca un correo electrónico para acceder:")
             time.sleep(1)
         elif hora_actual > "12:00:00.000000" and hora_actual < "21:00:00.000000":
-            print("Buenas tardes:\n Introduzca un usuario y un correo eléctronico para acceder:")
+            print("Buenas tardes:\nIntroduzca un correo electrónico para acceder:")
             time.sleep(1)
         else:
-            print("Buenas noches:\nIntroduzca un usuario y un correo eléctronico para acceder:")
+            print("Buenas noches:\nIntroduzca un correo electrónico para acceder:")
             time.sleep(1)
     def usuario():
         s=str(input("Usuario:\n"))
-        comprobacion = re.search(". * @. * \ .. *", s)
-        if comprobacion == True:
-            print("¡Bienvenido {s}!")
-        elif comprobacion == None:
-            print("Lo sentimos, el correo electrónico es erróneo.")
-            Correo_electronico.usuario()
-        else:
-            pass
-
+        print(re.search(". * @. * \ .. *", s))
 
 Correo_electronico.comprobar_hora()
 Correo_electronico.usuario()
