184506f9abc2385dc13bad68c0053b7cf6c61d27
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-22 13:28:23 +0100

    eliminado los dominios por ahora

--- Clases/Web.py
+++ Clases/Web.py
@@ -7,26 +7,24 @@ class Correo_electronico:
     def comprobar_hora():
         hora_actual=str(datetime.now().time())
         if hora_actual > "08:00:00.000000" and hora_actual < "12:00:00.000000":
-            print("Buenos días:\nIntroduzca un correo electrónico para acceder:")
+            print("Buenos días:\nIntroduzca un correo electrónico para acceder")
             time.sleep(1)
         elif hora_actual > "12:00:00.000000" and hora_actual < "21:00:00.000000":
-            print("Buenas tardes:\nIntroduzca un correo electrónico para acceder:")
+            print("Buenas tardes:\nIntroduzca un correo electrónico para acceder")
             time.sleep(1)
         else:
-            print("Buenas noches:\nIntroduzca un correo electrónico para acceder:")
+            print("Buenas noches:\nIntroduzca un correo electrónico para acceder")
             time.sleep(1)
     def usuario():
-        Correo_electronico.comprobar_hora()
-        dominios = ["com", "es", "org"]
         correo_electronico=str(input("Correo electrónico:\n"))
-        while len(dominios):
-            variable=dominios.pop(0)
-            s=str(input("Correo electrónico:\n"))
-            comprobacion=re.search(f"@......com", correo_electronico)
-            if comprobacion == None:
-                Correo_electronico.usuario()
-            else:
-                print(f"Bienvenido {s}")
+        correo_electronico
+        str(input("Correo electrónico:\n"))
+        comprobacion=re.search(f"@......com", correo_electronico)
+        if comprobacion == None:
+            Correo_electronico.usuario()
+        else:
+            print(f"Bienvenido {correo_electronico}")
 
 
 Correo_electronico.comprobar_hora()
+Correo_electronico.usuario()
