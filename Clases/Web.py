3e6444359f48e9a96a93aa85b7474e99f6f18e10
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-22 14:00:15 +0100

    cambios en los print y en el re.search

--- Clases/Web.py
+++ Clases/Web.py
@@ -7,21 +7,23 @@ class Correo_electronico:
     def comprobar_hora():
         hora_actual=str(datetime.now().time())
         if hora_actual > "08:00:00.000000" and hora_actual < "12:00:00.000000":
-            print("Buenos días:\nIntroduzca un correo electrónico para acceder")
+            print("Buenos días:\n")
             time.sleep(1)
         elif hora_actual > "12:00:00.000000" and hora_actual < "21:00:00.000000":
-            print("Buenas tardes:\nIntroduzca un correo electrónico para acceder")
+            print("Buenas tardes:\n")
             time.sleep(1)
         else:
-            print("Buenas noches:\nIntroduzca un correo electrónico para acceder")
+            print("Buenas noches:\n")
             time.sleep(1)
     def usuario():
         correo_electronico = str(input("Introduzca su correo electrónico:\n"))
-        comprobacion=re.search(f"@......com", correo_electronico)
+        comprobacion=re.search("@......", correo_electronico)
         if comprobacion == None:
+            print("Correo no válido, mantenga el formato xxx@xxx.xx\n")
             Correo_electronico.usuario()
         else:
-            print(f"Bienvenido {correo_electronico}")
+            usuario = re.sub("@gmail.com.es.orgyahoohotmail!","",correo_electronico)
+            print(f"Bienvenido {usuario}")
 
 
 Correo_electronico.comprobar_hora()
