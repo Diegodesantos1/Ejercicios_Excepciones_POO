1b4c7d9fc3bda8007786ee31d613f7c0c7c11e1c
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-22 10:33:17 +0100

    todavía no funciona, pero la función es recursiva, falta optimizar

--- Clases/Web.py
+++ Clases/Web.py
@@ -7,14 +7,25 @@ class Correo_electronico:
     def comprobar_hora():
         hora_actual=str(datetime.now().time())
         if hora_actual > "08:00:00.000000" and hora_actual < "12:00:00.000000":
-            print("Buenos días:\n Introduzca un usuario y un correo eléctronico para acceder:\n")
-            time.sleep(5)
+            print("Buenos días:\n Introduzca un usuario y un correo eléctronico para acceder:")
+            time.sleep(1)
         elif hora_actual > "12:00:00.000000" and hora_actual < "21:00:00.000000":
-            print("Buenas tardes:\n Introduzca un usuario y un correo eléctronico para acceder:\n")
-            time.sleep(5)
+            print("Buenas tardes:\n Introduzca un usuario y un correo eléctronico para acceder:")
+            time.sleep(1)
         else:
-            print("Buenas noches:\n Introduzca un usuario y un correo eléctronico para acceder:\n")
-            time.sleep(5)
+            print("Buenas noches:\nIntroduzca un usuario y un correo eléctronico para acceder:")
+            time.sleep(1)
     def usuario():
-        usuario=input("Usuario:\n")
+        s=str(input("Usuario:\n"))
+        comprobacion = re.search(". * @. * \ .. *", s)
+        if comprobacion == True:
+            print("¡Bienvenido {s}!")
+        elif comprobacion == None:
+            print("Lo sentimos, el correo electrónico es erróneo.")
+            Correo_electronico.usuario()
+        else:
+            pass
+
+
 Correo_electronico.comprobar_hora()
+Correo_electronico.usuario()
