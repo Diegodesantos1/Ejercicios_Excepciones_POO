5f54574ef6b9cbc0864601489abbb96bbd8bfefe
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-03-22 10:14:24 +0100

    importado time y re, y planteada la función usuario

--- Clases/Web.py
+++ Clases/Web.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-
+import time
+import re
 class Correo_electronico:
     def __init__(self, hora_actual):
         self.hora_actual = hora_actual
@@ -7,8 +8,13 @@ class Correo_electronico:
         hora_actual=str(datetime.now().time())
         if hora_actual > "08:00:00.000000" and hora_actual < "12:00:00.000000":
             print("Buenos días:\n Introduzca un usuario y un correo eléctronico para acceder:\n")
+            time.sleep(5)
         elif hora_actual > "12:00:00.000000" and hora_actual < "21:00:00.000000":
             print("Buenas tardes:\n Introduzca un usuario y un correo eléctronico para acceder:\n")
+            time.sleep(5)
         else:
             print("Buenas noches:\n Introduzca un usuario y un correo eléctronico para acceder:\n")
+            time.sleep(5)
+    def usuario():
+        usuario=input("Usuario:\n")
 Correo_electronico.comprobar_hora()
